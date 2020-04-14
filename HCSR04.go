b39af929e1c2850a736454911725bc8c66c460ef
Bryan Varner <1652015+bvarner@users.noreply.github.com>
2020-04-13 20:41:26 -0400

    Added the initial pass at making this thing.

---
+++ HCSR04.go
@@ -0,0 +1,93 @@
+package pidroponics
+
+import (
+	"log"
+	"math"
+	"periph.io/x/periph/conn/gpio"
+	"periph.io/x/periph/conn/gpio/gpioreg"
+	"periph.io/x/periph/host"
+	"sync"
+	"time"
+)
+
+type HCSR04 struct {
+	TriggerPin		gpio.PinIO	`json:"-"`
+	EchoPin			gpio.PinIO	`json:"-"`
+
+	sync.Mutex					`json:"-"`
+	triggeredWhen	*time.Time	`json:"-"`
+	echoStart		*time.Time	`json:"-"`
+	echoEnd			*time.Time	`json:"-"`
+
+	Distance		float64
+}
+
+func NewHCSR04(triggerPinName string, echoPinName string)(*HCSR04, error) {
+	var err error = nil
+	if _, err = host.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	h := &HCSR04{
+		TriggerPin: gpioreg.ByName(triggerPinName),
+		EchoPin: gpioreg.ByName(echoPinName),
+		Distance: math.NaN(),
+	}
+
+	// Set the trigger low by default.
+	h.TriggerPin.Out(gpio.Low)
+	// Set the echo up to listen.
+	err = h.EchoPin.In(gpio.PullDown, gpio.BothEdges)
+	if err == nil {
+		go func() {
+			var maxResult, _ = time.ParseDuration("38ms")
+			var unobstructed = maxResult.Microseconds()
+
+			for {
+				// On edge change of the echo...
+				h.EchoPin.WaitForEdge(-1)
+				var t = time.Now()
+				if h.echoStart == nil {
+					h.echoStart = &t
+				} else if h.echoEnd == nil {
+					h.echoEnd = &t
+				}
+
+				// compute the distance, clear the value.
+				if h.echoStart != nil && h.echoEnd != nil {
+					// compute this down to centimeters
+					var delay = h.echoEnd.Sub(*h.echoEnd).Microseconds();
+					if delay < unobstructed {
+						h.Distance = float64(delay) / 58
+					} else {
+						// Or if we can't detect things in front of us...
+						h.Distance = math.NaN()
+					}
+
+					// Clear Status
+					h.echoStart = nil;
+					h.echoEnd = nil;
+				}
+			}
+		}()
+	} else {
+		log.Print(err);
+	}
+
+	return h, err;
+}
+
+func (h *HCSR04) MeasureDistance() (error) {
+	h.Lock()
+	defer h.Unlock()
+
+	var pulse time.Duration = 0
+
+	pulse += 10 * time.Microsecond
+	h.TriggerPin.Out(gpio.Low)
+	h.TriggerPin.Out(gpio.High)
+	time.Sleep(pulse)
+	h.TriggerPin.Out(gpio.Low)
+
+	return nil
+}
---
+++ pidroponics/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	rice "github.com/GeertJohan/go.rice"
+	"github.com/bvarner/pidroponics"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+var handler http.Handler
+
+var inletWaterLevel *pidroponics.HCSR04
+
+var drainWaterLevel *pidroponics.HCSR04
+
+func redirectTLS(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://" + r.Host + r.RequestURI, http.StatusMovedPermanently)
+}
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	// Push some things if we know what our request is.
+	if r.URL.Path == "/" || r.URL.Path == "/index.html" {
+/*		p, ok := w.(http.Pusher)
+		if ok {
+			p.Push("/events", nil)
+			p.Push("/style.css", nil)
+			p.Push("/App.js", nil)
+		}
+*/	}
+
+	handler.ServeHTTP(w, r)
+}
+
+func main() {
+	var err error = nil
+
+	inletWaterLevel, err = pidroponics.NewHCSR04("GPIO5", "GPIO6")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Inlet Water Level Not Initialized: ", err);
+	}
+
+	drainWaterLevel, err = pidroponics.NewHCSR04("GPIO16", "GPIO17")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Drain Water Level Not Initialized: ", err);
+	}
+
+	poller := time.NewTicker(1 * time.Second)
+	go func() {
+		for range poller.C {
+			inletWaterLevel.MeasureDistance()
+			fmt.Println("Inlet Wather Level: ", inletWaterLevel.Distance)
+			drainWaterLevel.MeasureDistance()
+			fmt.Println("Drain Wather Level: ", drainWaterLevel.Distance)
+		}
+	}()
+
+	fmt.Println("Setting up HTTP server...")
+
+	handler = http.FileServer(rice.MustFindBox("webroot").HTTPBox())
+	fmt.Println("Found the rice box.")
+
+	// Setup the handlers.
+	http.HandleFunc("/", RootHandler)
+
+	// Setup the SSE Event Handler. This comes from the 'broker'.
+	//http.HandleFunc("/events", broker.ServeHTTP)
+
+	cert := flag.String("cert", "/etc/ssl/certs/pidroponics.pem", "The certificate for this server.")
+	certkey := flag.String("key", "/etc/ssl/certs/pidroponics.pem", "The key for the server cert.")
+
+	flag.Parse()
+
+	_, certerr := os.Stat(*cert)
+	_, keyerr := os.Stat(*certkey)
+
+	if certerr == nil && keyerr == nil {
+		fmt.Println("SSL Configuration set up.")
+		go func() {
+			log.Fatal(http.ListenAndServe(":80", http.HandlerFunc(redirectTLS)));
+		} ()
+		log.Fatal(http.ListenAndServeTLS(":443", *cert, *certkey, nil))
+	} else {
+		fmt.Println("SSL Configuration not found.")
+		log.Fatal(http.ListenAndServe(":80", nil))
+	}
+}
